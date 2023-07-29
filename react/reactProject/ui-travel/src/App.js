1ac55c1557655e95603925a5795e6d5c7ad6632f
Sohdai <soudai16@gmail.com>
2023-07-29 22:21:43 +0900

    commit

---
+++ react/reactProject/ui-travel/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PermanentDrawerLeft from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <PermanentDrawerLeft />
+    </div>
+  );
+}
+
+export default App;
